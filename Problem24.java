efe8c61089d082a4148e5fa693bca1d18515a7a5
yatharthdubey <46920224+yatharthdubey@users.noreply.github.com>
2019-01-23 16:34:31 +0530

    Add files via upload

---
+++ Problem24.java
@@ -0,0 +1,51 @@
+import java.util.HashMap;
+import java.util.Scanner;
+import java.util.Map.Entry;
+
+public class Problem24 {
+	
+	public static void main(String[] args) {
+		// highest average marks
+		HashMap<String, Student> hm = new HashMap<String, Student>();
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Number of Input");
+		int input = sc.nextInt();
+		while(input-- > 0) {
+			String name = sc.next();
+			int mark = sc.nextInt();
+			if(hm.containsKey(name)) {
+				Student obj = (Student) hm.get(name);
+				obj.marks += mark;
+				obj.numberOfSubject++;
+			}
+			else {
+				Student obj = new Student(name, mark);
+				hm.put(name, obj);
+			}
+		}
+		int max = 0;
+		String name=null;
+		for(Entry entry:hm.entrySet()) {
+			Student obj = (Student) entry.getValue();
+			int c = obj.marks / obj.numberOfSubject;
+			if(max < c) {
+				max = c;
+				name = (String) entry.getKey();
+			}
+		}
+		System.out.println((int)max);
+	}	
+}
+//class Student{
+//	String name=null;
+//	int marks=0;
+//	int numberOfSubject=0;
+//	
+//
+//
+//	public Student(String name, int marks) {
+//		this.name = name;
+//		this.marks += marks;
+//		this.numberOfSubject++;
+//	}
+//}
